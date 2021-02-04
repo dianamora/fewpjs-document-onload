2e91e605f77161579c3bf700949c158c70346ea7
Diana Mora <morra.dianna@gmail.com>
2021-02-04 11:36:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+//set up a DOMContentLoaded event in order to detect when the HTML page has loaded
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+  }); 
+
+//Use the event's callback function to target the paragraph element with id="text" 
+//and replace the text with "This is really cool!"
+
+function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
